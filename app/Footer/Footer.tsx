8c2c627f0e2f3b89ad2b43fa7efa701b8ca3ac7d
Julien-44100 <julienbotar44100@yahoo.com>
2025-06-14 20:57:55 +0200

    finished footer

---
+++ app/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+"use client";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+	return (
+		<nav className={styles.Footer}>
+			<img src="/Image/logogta6.png" alt="logo grand theft auto 6" className={styles.logofooter} />
+		</nav>
+	);
+}
